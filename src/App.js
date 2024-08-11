67187a30fb27559f389f7582fc404a273e56a902
phoenixlibbi <2021ee166@student.uet.edu.pk>
2024-08-11 08:41:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from "framer-motion";
+import React from "react";
+import { AuroraBackground } from "./ui/aurora-background.tsx";
+import { FlipWords } from "./ui/flip-words.tsx";
 
 function App() {
+
+  const words = ["Eco-Friendly Solutions",
+    "Advanced Cleaning Tech",
+    "High-Efficiency",
+    "Industrial Strength",
+    "Safe for Home Use",
+    "Surface Protection",
+    "Fast-Acting Formulas",
+    "Non-Toxic Ingredients",
+    "Odor Neutralization",
+    "Multi-Purpose Use",
+    "Long-Lasting",
+    "Cost-Effective Solutions",
+    "Heavy-Duty Cleaning",
+    "Stain Removal"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+        Meliora Products <br /> <span  className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4"><FlipWords words={words} /></span>
+        </div>
+        <div className="font-extralight text-base md:text-2xl dark:text-neutral-200 py-4">
+        Elevating Cleanliness, Empowering Excellence.
+        </div>
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          Shop now
+        </button>
+      </motion.div>
+    </AuroraBackground>
   );
 }
 
