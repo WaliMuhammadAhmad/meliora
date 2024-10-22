f5f01cc23c1654d87a836778f76678007b2d1aea
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-10-22 21:31:28 +0500

    router.js refactored

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,11 +1,8 @@
 const express = require('express');
-// const connection = require('../controllers/controller')
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Hello');
 })
-
-
 
 module.exports = router;
