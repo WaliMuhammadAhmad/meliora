f1305d6bdf42d60c4765d20db9fd2d6d5d5dd99d
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-10-24 08:01:23 +0500

    routes fix

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
-router.post('/', orderController.createOrder);
+router.post('/', orderController.neworder);
 router.get('/', orderController.getAllOrders);
 router.get('/:id', orderController.getOrderById);
 router.get('/:customerId', orderController.getOrderByCustomerId);
