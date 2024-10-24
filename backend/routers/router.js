72cfd0079d4c067b6907c07cc3278fbe091ae78f
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-10-24 08:02:54 +0500

    router updated with routes and added in server.js

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const adminRouter = require('./adminRouter');
+const customerRouter = require('./customerRouter');
+const productRouter = require('./productRouter');
+const packageRouter = require('./packageRouter');
+const reviewRouter = require('./reviewRouter');
+const orderRouter = require('./orderRouter');
+
+router.use('/admins', adminRouter);
+router.use('/products', productRouter);
+router.use('/customers', customerRouter);
+router.use('/package', packageRouter);
+router.use('/review', reviewRouter);
+router.use('/order', orderRouter);
 
 router.get('/', (req, res) => {
-    res.send('Hello');
-})
+    res.send('hello');
+});
+
+router.get('/test', (req, res) => {
+    res.send('Test works');
+});
 
 module.exports = router;
