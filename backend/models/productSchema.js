d9d509b4ff60ea958da2fc516c9768d8bd452b62
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-11-09 21:58:09 +0500

    update: field according to frontend

--- backend/models/productSchema.js
+++ backend/models/productSchema.js
@@ -8,6 +8,18 @@ const productSchema = new mongoose.Schema({
         minlength: [3, 'Product name must be at least 3 characters long'],
         maxlength: [100, 'Product name must not exceed 100 characters']
     },
+    detail: {
+        type: String,
+        required: [true, 'Product Detail is required'],
+        trim: true,
+        minlength: [10, 'Detail must be at least 10 characters long']
+    },
+    subDetail: {
+        type: String,
+        required: [true, 'Product sub-detail is required'],
+        trim: true,
+        minlength: [10, 'Sub-detail must be at least 10 characters long']
+    },
     description: {
         type: String,
         required: [true, 'Product description is required'],
@@ -43,7 +55,30 @@ const productSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'Stock quantity is required'],
         min: [0, 'Stock quantity cannot be negative'],
-        default: 0
+        default: 1
+    },
+    usageTitle: {
+        type: String,
+        required: [true, 'Product Usage title is required'],
+        trim: true,
+        minlength: [5, 'Product Usage title must be at least 5 characters long'],
+    },
+    steps : {
+        step1: {
+            title: {type: String, required: [true, 'Step 1 title is required'], trim: true},
+            description: {type: String, required: [true, 'Step 1 description is required'], trim: true},
+            image: {type: String, required: [true, 'Step 1 image is required'], trim: true}
+        },
+        step2: {
+            title: {type: String, required: [true, 'Step 2 title is required'], trim: true},
+            description: {type: String, required: [true, 'Step 2 description is required'], trim: true},
+            image: {type: String, required: [true, 'Step 2 image is required'], trim: true}
+        },
+        step3: {
+            title: {type: String, required: [true, 'Step 3 title is required'], trim: true},
+            description: {type: String, required: [true, 'Step 3 description is required'], trim: true},
+            image: {type: String, required: [true, 'Step 3 image is required'], trim: true}
+        }
     }
 }, { 
     timestamps: true
