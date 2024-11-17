e403cfd007257c6cc92263eb837328754256e668
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-11-17 09:48:44 +0500

    fix: multer static route for image req and res

--- backend/routers/adminRouter.js
+++ backend/routers/adminRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const upload = require('../middlewares/uploadAdminImage');
 
-router.post('/', upload.single('image'), adminController.createAdmin);
+router.post('/', upload.single('admin-image'), adminController.createAdmin);
 router.get('/', adminController.getAllAdmins);
 router.get('/:id', adminController.getAdminById);
-router.put('/:id', upload.single('image'), adminController.updateAdmin);
+router.put('/:id', upload.single('admin-image'), adminController.updateAdmin);
 router.put('/role/:id', adminController.updateAdminRole);
 router.delete('/:id', adminController.deleteAdmin);
 
