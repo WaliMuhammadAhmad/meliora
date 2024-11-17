f77f486e3e3df3b421743dd3edec82bf0597aead
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-11-17 09:53:10 +0500

    update: routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,31 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
 import Checkout from './pages/Chekcout';
-import Admin from './pages/Admin';
-import Dashboard from './pages/Dashboard';
-import Order from './pages/Order';
-import AddProducts from './pages/Product';
-import Blog from './pages/Blog';
-import Account from './pages/Account';
-import Packages from './pages/Packages';
+import Admin from './dashboard/pages/Admin';
+import Dashboard from './dashboard/pages/Dashboard';
+import Order from './dashboard/pages/Order';
+import AddProducts from './dashboard/pages/Product';
+import Blog from './dashboard/pages/Blog';
+import Account from './dashboard/pages/Account';
+import Packages from './dashboard/pages/Packages';
 import OrderDetails from './pages/OrderDetails';
 import { Navbar } from './components/Navbar';
-// import SignIn from './signin/SignIn';
-// import SignUp from './signup/SignUp';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
-  // {
-  //   path: '/signin',
-  //   element: <SignIn />,
-  // },
-  // {
-  //   path: 'signup',
-  //   element: <SignUp />,
-  // },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
   {
     path: "/product-details/:id",
     element: <ProductDetails />,
