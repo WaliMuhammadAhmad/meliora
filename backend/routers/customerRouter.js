48ff5cafb78a13c03c4109b94314366a5de748ed
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-11-23 10:18:42 +0500

    new: new routes added for APIs

--- backend/routers/customerRouter.js
+++ backend/routers/customerRouter.js
@@ -5,7 +5,9 @@ const customerController = require('../controllers/customerController');
 router.post('/', customerController.createCustomer);
 router.get('/', customerController.getAllCustomers);
 router.get('/:id', customerController.getCustomerById);
+router.get('/email/:email', customerController.getCustomerByEmail);
 router.put('/:id', customerController.updateCustomer);
+router.put('/updateAddress/:email', customerController.updateAddress);
 router.delete('/:id', customerController.deleteCustomer);
 
 module.exports = router;
