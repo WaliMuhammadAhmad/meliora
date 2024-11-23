287aa512019bb6489262704dbe76340f310e4c2c
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-11-23 12:51:11 +0500

    fix: class -> className

--- frontend/src/components/ProductUsage.jsx
+++ frontend/src/components/ProductUsage.jsx
@@ -3,22 +3,22 @@ import styles from "./style.module.css";
 
 export default function ProductUsage({ product }) {
   return (
-    <div class={styles.productcardscontainer}>
+    <div className={styles.productcardscontainer}>
       <h1>{product.usageTitle}</h1>
       <div className={styles.cards}>
-        <div class={styles.card}>
+        <div className={styles.card}>
           <img src={product.steps.step1.image} alt="Step 1" />
           <h3>{product.steps.step1.title}</h3>
           <p>{product.steps.step1.description}</p>
         </div>
 
-        <div class={styles.card}>
+        <div className={styles.card}>
           <img src={product.steps.step2.image} alt="Step 2" />
           <h3>{product.steps.step2.title}</h3>
           <p>{product.steps.step2.description}</p>
         </div>
 
-        <div class={styles.card}>
+        <div className={styles.card}>
           <img src={product.steps.step3.image} alt="Step 3" />
           <h3>{product.steps.step3.title}</h3>
           <p>{product.steps.step3.description}</p>
