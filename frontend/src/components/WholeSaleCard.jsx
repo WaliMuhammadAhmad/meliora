fa41644c1eaf19ccf2bd01c07aa8854dfd115b79
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-11-26 08:57:48 +0500

    update: cardMedia to img with crossOrigin

--- frontend/src/components/WholeSaleCard.jsx
+++ frontend/src/components/WholeSaleCard.jsx
@@ -1,30 +1,34 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+// import CardMedia from '@mui/material/CardMedia';
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 
-export default function WholeSaleCard({pkg}) {
+export default function WholeSaleCard({ pkg }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
+        <img
           height="70"
-          image={pkg.image}
+          src={pkg.image}
           alt={pkg.name}
+          crossOrigin={
+            pkg.image.startsWith("http://localhost:3001")
+              ? "anonymous"
+              : undefined
+          }
         />
         <CardContent>
-          <div className='flex justify-between'>
-          <Typography gutterBottom variant="h5" component="div">
-            {pkg.name}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            ${pkg.price}
-          </Typography>
+          <div className="flex justify-between">
+            <Typography gutterBottom variant="h5" component="div">
+              {pkg.name}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              ${pkg.price}
+            </Typography>
           </div>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
             {pkg.description}
           </Typography>
         </CardContent>
