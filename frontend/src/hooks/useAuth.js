88b7167376fc7b1f189ba32a60a01623ad0e1f69
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-11-27 22:34:28 +0500

    useAuth hook

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,24 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
 const useAuth = () => {
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const token = Cookies.get("accessToken");
-
-    if (storedUser && token) {
-      checkAuth();
-    } else {
-      setIsAuthenticated(false);
-      setUser(null);
-    }
-  }, []);
+  const isAccessTokenExpired = (token) => {
+    const decoded = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+    return decoded.exp < currentTime;
+  };
 
   const login = (userData, accessToken, refreshToken) => {
     localStorage.setItem("user", JSON.stringify(userData));
@@ -32,6 +26,7 @@ const useAuth = () => {
       expires: 7,
       secure: true,
       sameSite: "Strict",
+      httpOnly: true,
     });
 
     setUser(userData);
@@ -40,7 +35,6 @@ const useAuth = () => {
 
   const logout = () => {
     localStorage.removeItem("user");
-    localStorage.removeItem("lastPage");
     Cookies.remove("accessToken");
     Cookies.remove("refreshToken");
 
@@ -50,34 +44,39 @@ const useAuth = () => {
 
   const checkAuth = async () => {
     const accessToken = Cookies.get("accessToken");
+    const refreshToken = Cookies.get("refreshToken");
 
-    if (!accessToken) {
-      setIsAuthenticated(false);
-      setUser(null);
+    if (!refreshToken) {
+      logout();
       return;
     }
 
     try {
-      const response = await axios.get("/auth/token/verify", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
+      const refreshResponse = await axios.post("/auth/token/verify", {
+        token: refreshToken,
       });
 
-      if (response.data.message === "Token is valid") {
-        setIsAuthenticated(true);
-      } else if (response.data.message === "No token provided") {
-        console.error(response.data.message);
-        alert("Please Sign in or Sign up first!");
+      if (
+        refreshResponse.status === 200 &&
+        refreshResponse.data.message === "Token is valid"
+      ) {
+        if (isAccessTokenExpired(accessToken)) {
+          const newTokenResponse = await axios.post("/auth/token/refresh", {
+            token: refreshToken,
+          });
+
+          const newAccessToken = newTokenResponse.data.accessToken;
+          Cookies.set("accessToken", newAccessToken);
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(true);
+        }
       } else {
-        console.error(response.data.message);
-        setIsAuthenticated(false);
-        setUser(null);
+        logout();
       }
     } catch (error) {
-      // If there's an error (e.g., invalid token or expired token), log out the user
-      setIsAuthenticated(false);
-      setUser(null);
+      console.error("Error during authentication check:", error);
+      logout();
     }
   };
 
